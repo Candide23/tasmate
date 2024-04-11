d2ef48517f345a1fd6dc4596b7788de98b579bb1
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-04-11 13:18:49 -0500

    useState chapiter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './App.css'
 
 const App = () => {
+
+ const [count, setCount]  = useState(0);
+
+    function handleAdd() {
+    setCount(count+1)
+    }
+    function handleSub() {
+        setCount(count -1)
+
+    }
+
+    function handleReset() {
+        setCount(0 )
+
+    }
   return (
-   <h1>
-    Hello world!
-   </h1>
+    <div>
+        <h1 className="App">
+            <div className='box'>
+               <p> {count} </p>
+               <button onClick={handleAdd} className='add'>ADD</button>
+               <button onClick={handleSub} className='sub'>SUB</button>
+               <button onClick={handleReset} className='sub'>RESET</button>
+
+            </div>
+        </h1>
+
+      
+    </div>
   )
 }
 
 export default App
+
+
