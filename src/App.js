ba43d24583e540a5d28a01ac93ab7d6ff6f36be6
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-04-11 13:40:26 -0500

    Array state value

--- src/App.js
+++ src/App.js
@@ -3,34 +3,28 @@ import './App.css'
 
 const App = () => {
 
- const [count, setCount]  = useState(0);
+    const[tasks, setTasks] = useState([
+        {id:1, name: "Record Lectures", completed: true},
+        {id:2, name: "Edit React Lectures", completed: false}, 
+        {id:3, name: "Watch Lectures", completed: false}
+    ])
 
-    function handleAdd() {
-    setCount(count+1)
-    }
-    function handleSub() {
-        setCount(count -1)
-
-    }
-
-    function handleReset() {
-        setCount(0 )
-
-    }
+ 
   return (
-    <div>
-        <h1 className="App">
-            <div className='box'>
-               <p> {count} </p>
-               <button onClick={handleAdd} className='add'>ADD</button>
-               <button onClick={handleSub} className='sub'>SUB</button>
-               <button onClick={handleReset} className='sub'>RESET</button>
+        <div className="App">
+            <h1>My Tasks List</h1>
+            <ul>
+                {tasks.map( (task, index) => (
+                    <li key={index} >
+                        <span>{task.id}{task.name}</span>
+                        <button className='delete'>Delete</button>
+                        </li>
+                ))}
+            </ul>
+           
+        </div>
 
-            </div>
-        </h1>
 
-      
-    </div>
   )
 }
 
