10f67f7416b6650d3b8bdae6541859e8b8882461
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-04-22 09:08:04 -0500

    added controlled inputs

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -5,15 +5,28 @@ const AddTask = () => {
 
     const[taskValue, setTaskValue] = useState("")
 
-  
+
+    const handleChange = (event) =>{
+
+        setTaskValue(event.target.value);
+
+
+    }
+
+    const handleReset = () => {
+
+        setTaskValue("")
+
+    }
   return (
     <section className='addtask'>
         <form>
             <label htmlFor=" ">Task Name</label>
-            <input onChange={(event) => setTaskValue(event.target.value)} type='text' name='task' id='task' placeholder='Task Name' autoComplete='off' /><br/>
+            <input onChange={handleChange} type='text' name='task' id='task' placeholder='Task Name' autoComplete='off' value={taskValue} />
             <button  type='submit'>Add Task</button>
+            <span onClick={handleReset} className='reset' color='red'>Reset</span>
         </form>
-        <p>{taskValue.length}</p>
+        <p>{taskValue}</p>
 
 
     </section>
