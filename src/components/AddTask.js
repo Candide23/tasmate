b50d8ea0819cc185126bf52c0e46fa8d33d584f9
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-04-22 08:59:15 -0500

    added onChange Event

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./AddTask.css"
 
 const AddTask = () => {
+
+    const[taskValue, setTaskValue] = useState("")
+
+  
   return (
     <section className='addtask'>
         <form>
             <label htmlFor=" ">Task Name</label>
-            <input type='text' name='task' id='task' placeholder='Task Name' autoComplete='off' /><br/>
+            <input onChange={(event) => setTaskValue(event.target.value)} type='text' name='task' id='task' placeholder='Task Name' autoComplete='off' /><br/>
             <button  type='submit'>Add Task</button>
         </form>
+        <p>{taskValue.length}</p>
+
 
     </section>
   )
