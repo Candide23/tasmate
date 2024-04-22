069382141aa5fe49be4626de8919259e450b835c
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-04-22 13:24:52 -0500

    added new task

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React,{useState} from 'react';
 import './App.css'
 import Header from "./components/Header"
 import { TaskList } from './components/TaskList'
@@ -7,13 +8,16 @@ import AddTask from './components/AddTask'
 
 const App = () => {
 
+    const [tasks, setTasks] = useState([]);
 
  
   return (
         <div className="App">
             <Header/>
-            <AddTask/>
-            <TaskList />
+            <main>
+            <AddTask  tasks={tasks} setTasks={setTasks} />
+            <TaskList tasks={tasks} setTasks={setTasks}/>
+            </main>
             <Footer/>
            
         </div>
