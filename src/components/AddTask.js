40ae82b787194243a685d3e539551df0b7a892bc
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-04-24 07:50:20 -0500

    added useRef hook

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,18 +1,21 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import "./AddTask.css"
 
 const AddTask = ({tasks, setTasks}) => {
 
-    const[taskValue, setTaskValue] = useState("");
+    //const[taskValue, setTaskValue] = useState("");
     const[progress, setProgress] = useState(false);
+    const taskRef = useRef("");
 
 
     const handleChange = (event) =>{
-        setTaskValue(event.target.value);
+
+        console.log(taskRef.current.value);
     }
 
     const handleReset = () => {
-        setTaskValue("");
+        //setTaskValue("");
+        taskRef.current.value="";
         setProgress(false);
     }
 
@@ -20,7 +23,7 @@ const AddTask = ({tasks, setTasks}) => {
         event.preventDefault();  //prevents page
         const task = {
             id: Math.floor(Math.random() * 1000),
-            name: taskValue,
+            name: taskRef.current.value,
             completed: Boolean(progress)
         }
         setTasks([...tasks, task])
@@ -32,7 +35,7 @@ const AddTask = ({tasks, setTasks}) => {
     <section className='addtask'>
         <form onSubmit={handleSubmit}> 
             <label htmlFor=" ">Task Name</label>
-            <input onChange={handleChange} type='text' name='task' id='task' placeholder='Task Name' autoComplete='off' value={taskValue} />
+            <input onChange={handleChange} type='text' name='task' id='task' placeholder='Task Name' autoComplete='off' ref={taskRef} />
             <select onChange={(event) => setProgress(event.target.value)} value={progress}>
                 <option value={false}>Pending</option>
                 <option value={true}>Completed</option>
@@ -40,7 +43,7 @@ const AddTask = ({tasks, setTasks}) => {
             <button  type='submit'>Add Task</button>
             <span onClick={handleReset} className='reset' color='red'>Reset</span>
         </form>
-        <p>{taskValue}</p>
+        <p></p>
 
 
     </section>
